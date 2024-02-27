3a3910d4ceb31e132f5f314774d04f55894670ec
Alex Myers <amyers3000@outlook.com>
2024-02-26 20:55:12 -0600

    Initial Commit

---
+++ Project-Exam-1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project-Exam-1
+//
+//  Created by Alex Myers on 2/26/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
